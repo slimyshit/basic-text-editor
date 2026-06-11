01c210f6d0223685f10b3fb5dbc51105bf1e9202
slimyshit <zainulabideeny2k8@gmail.com>
2026-06-11 11:50:54 +0500

    .

--- main.c
+++ main.c
@@ -4,7 +4,10 @@
 
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+
 	SDL_Window* window;
 
 	window = SDL_CreateWindow(
@@ -12,15 +15,36 @@ int main()
 		640,
 		480,
 		SDL_WINDOW_RESIZABLE
-		);
+	);
+
+	if (window == NULL)
+	{
+		printf("Window could not be created %s\n", SDL_GetError());
+		SDL_Quit();
+	}
+
 	bool done = false;
 	SDL_Event event;
+	SDL_Renderer* render;
+	render = SDL_CreateRenderer(window, NULL);
+
+	if (render == NULL)
+	{
+		printf("Render could not be created %s\n", SDL_GetError());
+		SDL_DestroyWindow(window);
+		SDL_Quit();
+	}
+
+
 	while (!done) {
 		
 		while (SDL_PollEvent(&event))
 			if (event.type == SDL_EVENT_QUIT) {
-				done == true;
+				done = true;
 			}
+		SDL_SetRenderDrawColor(render, 54, 56, 64, 1.0);
+		SDL_RenderClear(render);
+		SDL_RenderPresent(render);
 	}
 	SDL_DestroyWindow(window);
 	SDL_Quit();
